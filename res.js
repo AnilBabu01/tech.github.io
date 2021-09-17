e1f6ac208a7be948f45bc8b6f320088a04125f96
Anilpateel <90528633+Anilpateel@users.noreply.github.com>
2021-09-17 19:02:09 +0530

    second responsive website
    second responsive website

---
+++ res.js
@@ -0,0 +1,75 @@
+
+burgerr=document.querySelector(".burger");
+
+
+
+navbarr=document.querySelector(".navbar");
+
+rightnavv = document.querySelector('.rightnav')
+navlistt = document.querySelector('.nav-list');
+
+burgerr.addEventListener("click", function(){
+  
+
+rightnavv.classList.toggle('v-vis');
+navlistt.classList.toggle('v-vis');
+navbarr.classList.toggle('h-nav');
+
+});
+
+hidd=document.querySelector(".hide")
+
+
+hidd.addEventListener("click", function(){
+  
+
+rightnavv.classList.toggle('v-vis');
+navlistt.classList.toggle('v-vis');
+navbarr.classList.toggle('h-nav');
+
+});
+
+hidd1=document.querySelector(".hide1")
+
+
+hidd1.addEventListener("click", function(){
+  
+
+rightnavv.classList.toggle('v-vis');
+navlistt.classList.toggle('v-vis');
+navbarr.classList.toggle('h-nav');
+
+});
+
+
+hidd2=document.querySelector(".hide2")
+
+
+hidd2.addEventListener("click", function(){
+  
+
+rightnavv.classList.toggle('v-vis');
+navlistt.classList.toggle('v-vis');
+navbarr.classList.toggle('h-nav');
+
+});
+
+
+hidd3=document.querySelector(".hide3")
+
+
+hidd3.addEventListener("click", function(){
+  
+
+rightnavv.classList.toggle('v-vis');
+navlistt.classList.toggle('v-vis');
+navbarr.classList.toggle('h-nav');
+
+});
+
+
+
+
+
+
+
